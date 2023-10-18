74cf50336788f17a9229953afbe67945a4b1a633
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-19 01:37:20 +0530

    input fields styled

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,22 +9,24 @@ const Login = () => {
       <input className="modal-state" id="modal-1" type="checkbox" />
       <div className="modal">
         <label className="modal-overlay" htmlFor="modal-1"></label>
-        <div className="modal-content flex w-full flex-col gap-5 p-7">
+        <div className="modal-content flex w-1/2 flex-col gap-2 p-7">
           <label htmlFor="modal-1" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
-          <div className="flex flex-col gap-2">
-            <img src={require('../assets/logo.png')} alt="logo" className="w-2/6 self-center"/>
+          <div className="flex flex-col gap-9">
+            <img src={require('../assets/logo.png')} alt="logo" className="w-2/6 self-center mt-11" />
             <h2 className="text-start text-[28px] text-dark">Login</h2>
           </div>
 
           <section>
             <div className="form-group">
+              {/* email */}
               <div className="form-field">
-                <input placeholder="Email address" type="email" className="input max-w-full rounded-md" />
+                <input placeholder="Email address" type="email" className="input max-w-full rounded-md text-sm border-grey border" />
               </div>
 
               <div className="form-field">
+                {/* password */}
                 <div className="form-control">
-                  <input placeholder="Password" type="password" className="input max-w-full rounded-md" />
+                  <input placeholder="Password" type="password" className="input max-w-full rounded-md text-sm border-grey border" />
                 </div>
               </div>
 
@@ -33,14 +35,16 @@ const Login = () => {
               </div>
 
               <div className="form-field pt-5">
-                  <button type="button" className="btn w-full bg-pink-dark rounded-md text-white shadow-xl">SIGN IN</button>
+                {/* sign in */}
+                <button type="button" className="btn w-full bg-pink-dark rounded-md text-white shadow-xl">SIGN IN</button>
                 <span className="text-pink-dark text-xs font-medium">Your password or username is incorrect</span>
               </div>
             </div>
-            
+
             {/* dont have account */}
             <div className="items-center justify-center text-xs dark:text-gray-5 sm:px-6 flex gap-2">
               <span className="text-dark font-medium">Don't have an account?</span>
+              {/* register acc */}
               <a rel="/#" href="/#" className="link text-pink-dark font-medium text-xs">Create an account</a>
             </div>
           </section>
