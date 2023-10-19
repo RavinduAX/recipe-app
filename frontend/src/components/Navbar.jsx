0683ac67e335d598f2d8b60fa94abefc38cd0a98
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-19 14:41:08 +0530

    navbar styled and responsived

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { UserCircleIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
   return (
-    <div class="navbar">
+    <div class="navbar p-wrapper">
       <div class="navbar-start">
-        <img src={require('../assets/logo.png')} alt="logo" className="w-20" />
+        <img src={require('../assets/logo.png')} alt="logo" className="w-20 lg:w-[84px]" />
       </div>
       <div class="navbar-center">
-        <a class="navbar-item">HOME</a>
-        <a class="navbar-item">FAVOURITE</a>
+        <a href="/#" class="navbar-item text-sm">HOME</a>
+        <a href="/#" class="navbar-item text-sm">FAVOURITE</a>
       </div>
       <div class="navbar-end">
-        <a class="navbar-item">Home</a>
+        <UserCircleIcon className="h-7 w-7" />
       </div>
     </div>
   );
