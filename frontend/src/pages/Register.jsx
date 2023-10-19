7387fed6242d0a117d3933a1f18602b4b38ac616
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-19 12:04:08 +0530

    created register page & styled

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <article>
+      {/* button */}
+      <label className="btn btn-primary" htmlFor="modal-1">Open Modal</label>
+      {/* login screen */}
+      <input className="modal-state" id="modal-1" type="checkbox" />
+      <div className="modal">
+        <label className="modal-overlay" htmlFor="modal-1"></label>
+        <div className="modal-content flex w-5/6 flex-col gap-2 px-12 pb-14">
+          <label htmlFor="modal-1" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
+          <div className="flex flex-col gap-9">
+            <img src={require('../assets/logo.png')} alt="logo" className="w-2/6 self-center mt-11" />
+            <h2 className="text-start text-[28px] text-dark">Register</h2>
+          </div>
+
+          <section>
+            <div className="form-group">
+
+              {/* name */}
+              <div className="form-field sm:flex-row sm:gap-3">
+                <input placeholder="First name" type="text" className="input max-w-full rounded-md text-sm border-grey border" />
+                <input placeholder="Last name" type="text" className="input max-w-full rounded-md text-sm border-grey border" />
+              </div>
+
+              {/* email, contact no */}
+              <div className="form-field sm:flex-row sm:gap-3">
+                <input placeholder="Email address" type="email" className="input max-w-full rounded-md text-sm border-grey border" />
+                <input placeholder="Contact no" type="tel" className="input max-w-full rounded-md text-sm border-grey border" />
+              </div>
+
+              {/* password */}
+              <div className="form-field">
+                <div className="form-control flex-col sm:flex-row sm:gap-3">
+                  <input placeholder="Password" type="password" className="input max-w-full rounded-md text-sm border-grey border" />
+                  <input placeholder="Confirm Password" type="password" className="input max-w-full rounded-md text-sm border-grey border" />
+                </div>
+                <span className="text-pink-dark text-xs font-medium">Please enter a password</span>
+              </div>
+
+              <div className="form-field pt-2">
+                {/* sign in */}
+                <button type="button" className="btn w-full md:w-max bg-pink-dark rounded-md text-white shadow-xl text-sm">Create Account</button>
+              </div>
+            </div>
+
+            {/* dont have account */}
+            <div className="items-center justify-center text-xs dark:text-gray-5 sm:px-6 flex gap-2 mt-8">
+              <span className="text-dark font-medium">Already have an account?</span>
+              {/* register acc */}
+              <a rel="/#" href="/#" className="link text-pink-dark font-medium text-xs">Login</a>
+            </div>
+          </section>
+        </div>
+      </div>
+    </article>
+  )
+};
+
+export default Register;
