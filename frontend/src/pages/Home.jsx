9c0459132562b254fff23297c86a76fb59a7c002
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-20 00:00:36 +0530

    button group in home page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
+
+  const [category, setCategory] = useState('')
+
+
+
   return <div>
     <Navbar />
     <section className="bg-pink-light h-[calc(100vh-60px)] lg:h-[calc(100vh-64px)]">
-
+      {/* button group */}
+      <div className="btn-group btn-group-scrollable gap-3">
+        <input type="radio" name="category" data-content="Pork" className="btnCategory" value='pork' onChange={(e)=>{setCategory(e.target.value)}} defaultChecked/>
+        <input type="radio" name="category" data-content="Beaf" className="btnCategory" value='beaf' onChange={(e)=>{setCategory(e.target.value)}}/>
+        <input type="radio" name="category" data-content="Chicken" className="btnCategory" value='chicken' onChange={(e)=>{setCategory(e.target.value)}}/>
+        <input type="radio" name="category" data-content="Lamb" className="btnCategory" value='lamb' onChange={(e)=>{setCategory(e.target.value)}}/>
+        <input type="radio" name="category" data-content="Pasta" className="btnCategory" value='pasta' onChange={(e)=>{setCategory(e.target.value)}}/>
+      </div>
+    
     </section>
   </div>;
 };
