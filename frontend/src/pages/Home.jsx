31f71605e0d380618a16e255794f2e76cc3f8b84
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-19 15:48:39 +0530

    Home page updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,10 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
   return <div>
-    <Navbar/>
+    <Navbar />
+    <section className="bg-pink-light h-[calc(100vh-60px)] lg:h-[calc(100vh-64px)]">
+
+    </section>
   </div>;
 };
 
