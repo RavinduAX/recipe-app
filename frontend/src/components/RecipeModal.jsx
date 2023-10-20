abb2d16f010e90cbf6475a9015a07ab27362c0f2
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-20 13:55:50 +0530

    modal component main skeleton done

--- frontend/src/components/RecipeModal.jsx
+++ frontend/src/components/RecipeModal.jsx
@@ -16,12 +16,23 @@ const RecipeModal = () => {
             <span className="text-sm text-grey">#Japanese</span>
           </div>
           <div>
-            <div></div>
+            {/* left */}
+            <div>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXL4M9tdr6AgJR8Ub-o9lS2_7HIKFa3yq66NhUn7RG&s" alt="recipeImage" />
+              {/* ingredients */}
+              <ul>
+                <li className="flex gap-10">
+                  <p className="font-semibold">Soy Souce</p>
+                  <p className="">3/4 cup</p>
+                </li>
+              </ul>
+            </div>
+            {/* right */}
             <div>
               <span>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolorum voluptate ratione dicta. Maxime cupiditate, est commodi consectetur earum iure, optio, obcaecati in nulla saepe maiores nobis iste quasi alias!
               </span>
-              <a href="/#" className="btn">WATCH</a>
+              <a href="/#" className="btn w-32 h-9 bg-transparent text-pink-dark border-2 border-pink-dark hover:text-white hover:bg-pink-dark">WATCH</a>
             </div>
           </div>
         </div>
