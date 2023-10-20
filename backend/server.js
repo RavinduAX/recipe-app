8eb5b3f4623791b7cd81b486b791cdeb40488bfb
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-21 01:14:53 +0530

    connect db with backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
-
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT;
+const DB_URI = process.env.DB_URL;
 
 //express app
 const app = express();
@@ -12,7 +14,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//connect with db
+mongoose.connect(DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      logger.info(`Server UP : ${PORT}`);
+    })
+    logger.info('DB Connected')
+  })
+  .catch((err) => {
+    logger.error(err.message)
+  })
 //listen for requests
-app.listen(PORT, () => {
-  console.log(`Server UP : ${PORT}`);
-})
