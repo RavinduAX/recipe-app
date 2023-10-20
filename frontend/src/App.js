6dd8998be7bbd94d20ae1a7ff99bbd161c670767
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-10-20 10:48:53 +0530

    favourite page done & recipe card updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Favourite from './pages/Favourite';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -8,7 +9,8 @@ function App() {
     <div>
       {/* <Login/> */}
       {/* <Register/> */}
-      <Home/>
+      {/* <Home/> */}
+      <Favourite/>
     </div>
   );
 }
