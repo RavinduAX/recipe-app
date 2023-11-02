cbf476dd10b251310c6778962d001f2d12627745
Ravindu Adassuriya <ravinduadassuriya59@gmail.com>
2023-11-02 21:30:31 +0530

    home component button group udpated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,45 +4,28 @@ import RecipeCard from "../components/RecipeCard";
 
 const Home = () => {
 
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState('pork')
+
+  const catHandler = (cat) => {
+    setCategory(cat)
+  }
 
   return <div>
     <Navbar />
     <section className="bg-pink-light h-max px-5 lg:px-32 pt-8 pb-20">
       {/* button group */}
       <div className="btn-group btn-group-scrollable gap-3 mb">
-        <input type="radio" name="category" data-content="Pork" className="btnCategory" value='pork' onChange={(e)=>{setCategory(e.target.value)}} defaultChecked/>
-        <input type="radio" name="category" data-content="Beaf" className="btnCategory" value='beaf' onChange={(e)=>{setCategory(e.target.value)}}/>
-        <input type="radio" name="category" data-content="Chicken" className="btnCategory" value='chicken' onChange={(e)=>{setCategory(e.target.value)}}/>
-        <input type="radio" name="category" data-content="Lamb" className="btnCategory" value='lamb' onChange={(e)=>{setCategory(e.target.value)}}/>
-        <input type="radio" name="category" data-content="Pasta" className="btnCategory" value='pasta' onChange={(e)=>{setCategory(e.target.value)}}/>
+        <input onClick={(e) => { catHandler("pork") }} type="radio" name="category" data-content="Pork" className="btnCategory" defaultChecked />
+        <input onClick={() => { catHandler("beaf") }} type="radio" name="category" data-content="Beaf" className="btnCategory" />
+        <input onClick={() => { catHandler("chicken") }} type="radio" name="category" data-content="Chicken" className="btnCategory" />
+        <input onClick={() => { catHandler("lamb") }} type="radio" name="category" data-content="Lamb" className="btnCategory" />
+        <input onClick={() => { catHandler("pasta") }} type="radio" name="category" data-content="Pasta" className="btnCategory" />
       </div>
 
       <section className="flex flex-wrap items-center justify-between lg:gap-x-10 gap-y-10 mt-14">
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
-        <RecipeCard/>
+        <RecipeCard />
       </section>
-    
+
     </section>
   </div>;
 };
